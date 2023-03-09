cc5671fd653554cad9d6b9e5fd83bcbc3ec09587
arnoschn <arnoschn@users.noreply.github.com>
2023-03-09 12:13:33 +0100

    Update transformations.ts

--- src/http/schemas/transformations.ts
+++ src/http/schemas/transformations.ts
@@ -1,6 +1,7 @@
 export const transformationOptionsSchema = {
   height: { type: 'integer', examples: [100], minimum: 0 },
   width: { type: 'integer', examples: [100], minimum: 0 },
+  dpr: { type: 'integer', examples: [1], minimum: 1 },
   resize: { type: 'string', enum: ['cover', 'contain', 'fill'] },
   format: { type: 'string', enum: ['origin', 'avif', 'webp', 'jpeg', 'png'] },
   quality: { type: 'integer', minimum: 20, maximum: 100 },
